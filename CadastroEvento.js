d6ede52218eefb2a9763c8d3a69b087899662a36
Robson <robsonvieira250@hotmail.com>
2022-01-17 22:17:01 -0300

    Criacao Cadastro Evento

---
+++ CadastroEvento.js
@@ -0,0 +1,40 @@
+//Verificação data
+
+let Data = 18
+let DataHoje = 17
+
+if (Data > DataHoje){
+    console.log ("Data do evento posterior a atual, cadastro permitido!")
+} else{
+    console.log ("Data invalida")
+}
+
+//Verificação de idade
+
+let idade = 18
+
+if (idade>=18){
+    console.log ("Idade igual ou superior a 18 anos, cadastro permitido!")
+} else{
+    console.log ("Idade inferior  18 anos, cadastro não permitido")
+}
+
+//Verificação numeros de participantes
+
+let Participantes = 100
+
+if (Participantes<=100){
+    console.log ("Número de participantes é satisfatório, permitir cadastro!")
+} else{
+    console.log ("Número de Participantes excedido, não terá cadastro.")
+}
+
+//Verificação numeros de palestrante
+
+let Palestrante = 10
+
+if (Palestrante <=10){
+    console.log ("Número de Palestrante é satisfatório, permitir cadastro!")
+} else{
+    console.log ("Número de Palestrante excedido, não terá cadastro.")
+}
